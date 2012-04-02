49dac691ecb0dc4ab1e642643108e12284ca8b86
The.Wyrm.of.Essex@gmail.com <The.Wyrm.of.Essex@gmail.com@c5c38f73-7319-fe72-4605-3679ddb197a4>
2012-04-02 15:22:33 +0000

    Fix for home scripts using a homescripts script file. Writes out a number of skin strings containing all required info to display script on home screen. Scripts only. (XBMC XBox)

--- trunk/xTV-SAF/scripts/HomeScripts.py
+++ trunk/xTV-SAF/scripts/HomeScripts.py
@@ -1,11 +1,14 @@
 import xbmc,xbmcgui
 import os,sys
 
-SCRIPTDIR = 'special://xbmc/scripts'
-SKINFILESDIR = 'special://skin/PAL'
-FILEMODE = 'w'
+ScriptDir = 'special://xbmc/scripts'
 
 class Main:
+
+	# Get skin string name
+	SkinStringName = string.replace(str(sys.argv[1:]),"'","")
+	SkinStringName = string.replace(SkinStringName,"[","")
+	SkinStringName = string.replace(SkinStringName,"]","")
 
 	# Get the directory contents and put them in **ScriptList**
 	ScriptList = os.listdir(SCRIPTDIR)
@@ -15,40 +18,18 @@ class Main:
 	Listnum = dialog.select('Choose a Script', ScriptList)
 
 	try:
-		SCRIPTNAME = ScriptList [Listnum]
+		ScriptName = ScriptList [Listnum]
 		
 	except IndexError:
 		sys.exit(2)
 	else:		
-		SCRIPTLOCATION = os.path.join(SCRIPTDIR, SCRIPTNAME)
-		SCRIPTICON = os.path.join(SCRIPTLOCATION, 'default.tbn')
+		ScriptLocation = os.path.join(ScriptDir, ScriptName)
+		ScriptIcon = os.path.join(ScriptLocation, 'default.tbn')
 	
 		# Write Script name and location to skin variables
-		xbmc.executebuiltin("XBMC.Skin.SetString(home-scriptname," + SCRIPTNAME + ")")
-		xbmc.executebuiltin("XBMC.Skin.SetString(home-scriptlocation," + SCRIPTLOCATION + ")")
-		print SCRIPTICON,SCRIPTLOCATION
-	
-	try:
-		INCLUDESCRIPT = os.path.join(SKINFILESDIR, "HomeMenuScript.xml")
- 		f = open(xbmc.translatePath(INCLUDESCRIPT), FILEMODE)
-		
-	except IOError:
-		xbmcgui.Dialog().ok('could not open file ',INCLUDESCRIPT)
-	else:	
-		# Write out the conditional include file
-		f.write('<includes>' + '\n')
-		f.write('	<include name="homescreenscript">' + '\n')
-		f.write('		<icon>' + SCRIPTICON + '</icon>' + '\n')
-		f.write('		<thumb>' + SCRIPTICON + '</thumb>' + '\n')
-		f.write('		<label>' + SCRIPTNAME + '</label>' + '\n')
-		f.write('	</include>' + '\n')
-		f.write('</includes>' + '\n')
-			
-		f.flush()
-		f.close()
-		del f
-		
-	xbmc.executebuiltin('XBMC.ReloadSkin()')
+		xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-scriptloc," + ScriptLocation + ")")
+		xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-scripticon," + ScriptIcon + ")")
+		xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-scriptname," + ScriptName + ")")
 
 if ( __name__ == "__main__" ):
     Main()
