c25dbc646c15f8a82eb72761423edc2a1f7a8cbb
The.Wyrm.of.Essex@gmail.com <The.Wyrm.of.Essex@gmail.com@c5c38f73-7319-fe72-4605-3679ddb197a4>
2012-04-05 04:41:56 +0000

    Corrected home scripts script used in Custom HomeScripts feature. (XBMC XBox)

--- trunk/xTV-SAF/scripts/HomeScripts.py
+++ trunk/xTV-SAF/scripts/HomeScripts.py
@@ -4,37 +4,40 @@
 
 # main import's
 import xbmc,xbmcgui
-import os,sys
+import fileinput,os,sys,re,string
 
-ScriptDir = 'special://xbmc/scripts'
+SCRIPTDIR = 'special://xbmc/scripts'
 
-class Main:
-
-	# Get skin string name
-	SkinStringName = string.replace(str(sys.argv[1:]),"'","")
-	SkinStringName = string.replace(SkinStringName,"[","")
-	SkinStringName = string.replace(SkinStringName,"]","")
-
+def getscriptname():
 	# Get the directory contents and put them in **ScriptList**
 	ScriptList = os.listdir(SCRIPTDIR)
 	
 	# Prompt User for script name
 	dialog = xbmcgui.Dialog()
-	Listnum = dialog.select('Choose a Script', ScriptList)
-
+	Listnum = dialog.select(xbmc.getLocalizedString(31444), ScriptList)
 	try:
 		ScriptName = ScriptList [Listnum]
 		
 	except IndexError:
 		sys.exit(2)
-	else:		
-		ScriptLocation = os.path.join(ScriptDir, ScriptName)
-		ScriptIcon = os.path.join(ScriptLocation, 'default.tbn')
+	else:
+		return ScriptName
+
+def main():
+	SkinStringName = string.replace(str(sys.argv[1:]),"'","")
+	SkinStringName = string.replace(SkinStringName,"[","")
+	SkinStringName = string.replace(SkinStringName,"]","")
+
+	ScriptName = getscriptname()
 	
-		# Write Script name and location to skin variables
+	ScriptLocation = os.path.join(SCRIPTDIR, ScriptName)
+	ScriptIcon = os.path.join(ScriptLocation, 'default.tbn')
+
+	# Write out Skin strings with script info
 		xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-scriptloc," + ScriptLocation + ")")
 		xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-scripticon," + ScriptIcon + ")")
 		xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-scriptname," + ScriptName + ")")
 
-if ( __name__ == "__main__" ):
-    Main()
+if __name__ == '__main__':
+	main()	
+
