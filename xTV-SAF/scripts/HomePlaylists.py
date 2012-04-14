471e43f6a5b34135477397fe6eb0a646437a23a5
The.Wyrm.of.Essex <The.Wyrm.of.Essex@gmail.com>
2012-04-14 15:39:02 +0000

    Script for custom playlists on home screen. (XBMC XBox)

---
+++ xTV-SAF/scripts/HomePlaylists.py
@@ -0,0 +1,78 @@
+# HomePlugins script by Wyrm (xTV-SAF) #
+# Prompt user for name of playlist and output skin strings #
+# containing required info to run playlist from the home screen #
+
+# main import's
+import xbmc,xbmcgui
+import fileinput,os,sys,re,string
+
+PLAYLISTDIR = 'special://skin/extras/playlists'
+
+def getplaylisttype():
+	# Get the directory contents and put them in **MainPlaylistList**
+	MainPlaylistList = os.listdir(PLAYLISTDIR)
+	
+	# Prompt User for playlist type
+	dialog = xbmcgui.Dialog()
+	Listnum = dialog.select(xbmc.getLocalizedString(20006), MainPlaylistList)
+
+	try:
+		PlaylistTYPE = MainPlaylistList [Listnum]
+		
+	except IndexError:
+		sys.exit(2)
+	else:
+		return PlaylistTYPE
+
+def getplaylistname(Location):
+	# Get the directory contents and put them in **PlaylistList**
+	PlaylistList = os.listdir(Location)
+	
+	# Prompt User for playlist name
+	dialog = xbmcgui.Dialog()
+	Listnum = dialog.select(xbmc.getLocalizedString(656), PlaylistList)
+
+	try:
+		PlaylistNAME = PlaylistList [Listnum]
+		
+	except IndexError:
+		sys.exit(2)
+	else:
+		return PlaylistNAME
+		
+def getplaylisticon(ListType):
+	# Assign a Icon to the playlist based on if it was in Video or Music
+	# sub directory of skins playlist directory
+		if 'music' in ListType:
+			IconName = 'DefaultAudio.png'
+			return IconName
+
+		elif 'video' in ListType:
+			IconName = 'DefaultMovies.png'
+			return IconName
+
+		else:
+			sys.exit(2)
+		
+def main():
+	SkinStringName = string.replace(str(sys.argv[1:]),"'","")
+	SkinStringName = string.replace(SkinStringName,"[","")
+	SkinStringName = string.replace(SkinStringName,"]","")
+
+	PlaylistType = getplaylisttype()
+	PlaylistLOCATION = os.path.join(PLAYLISTDIR, PlaylistType)
+	Playlist = getplaylistname(PlaylistLOCATION)
+	
+	PlaylistLabel = string.replace(Playlist,".xsp","")
+
+	PlaylistIcon = getplaylisticon(PlaylistType)
+
+	# Write out Skin strings with plugin info
+	xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-playlistloc," + Playlist + ")")
+	xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-playlistlabel," + PlaylistLabel + ")")
+	xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-playlisticon," + PlaylistIcon + ")")
+	xbmc.executebuiltin("XBMC.Skin.SetString(" + SkinStringName + "-playlisttype," + PlaylistType + ")")
+
+if __name__ == '__main__':
+	main()	
+
